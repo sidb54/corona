4eb007dd545ca3d7ed4fa677e5ed67c5fcdbf177
Siddharth Behera <siddharthbehera.gyana@gmail.com>
2020-04-30 21:38:23 +0530

    first commit

---
+++ static/js/script3.js
@@ -0,0 +1,5 @@
+// if there is only one line of content you can put it with one less container: set margin-top = - line-height/2. ==> based on http://codeitdown.com/css-circles/ of Jose Vargas
+//rest of example based on https://codepen.io/JTLR/pen/JfbuA
+//article can be a div, an li or any other element
+//play wiht the .circle:before margin-right or margin-top
+//attention: estrange behaviour of text in last circle due to space separation in html between the  <div class="circle"> and the <h1> ¿any idea why?
